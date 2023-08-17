a8266a3a047f50d94a8d40fb3c778406f0d0c326
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-08-17 18:30:51 +0530

    Initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import App from "./App.jsx";
-// import { TransactionsProvider } from "./context/TransactionContext";
-// // import "./index.css";
-// import "./index.css";
-
-// ReactDOM.render(
-//   <TransactionsProvider>
-//     <App />
-//   </TransactionsProvider>,
-//   document.getElementById("root"),
-// );
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.js";
+import App from "./App.jsx";
 import { TransactionsProvider } from "./context/TransactionContext";
 // import "./index.css";
 import "./index.css";
