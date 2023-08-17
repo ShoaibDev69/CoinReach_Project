73a86bd10c8e66f8f1b04b23b40220a10ae56eca
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-08-17 18:32:57 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App.jsx";
+import App from "./App";
 import { TransactionsProvider } from "./context/TransactionContext";
-// import "./index.css";
 import "./index.css";
 
 ReactDOM.render(
   <TransactionsProvider>
     <App />
   </TransactionsProvider>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
