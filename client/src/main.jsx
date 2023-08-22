ae684e76964c75fa1c46723b2d7deed46f82c573
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-08-22 18:46:29 +0530

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
-const App = require("./App.jsx");
+const App = require("./App");
 const { TransactionsProvider } = require("./context/TransactionContext");
 require("./index.css");
 
