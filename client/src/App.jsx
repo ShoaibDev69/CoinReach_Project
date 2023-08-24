3913a3483cfc9d96c86f6434ea6942a037df0d1b
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-08-24 17:41:24 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import Navbar from "./components/Navbar";
-import Welcome from "./components/Welcome";
-import Footer from "./components/Footer";
-import Services from "./components/Services";
-import Transactions from "./components/Transactions";
+const React = require("react");
+const Navbar = require("./components/Navbar");
+const Welcome = require("./components/Welcome");
+const Footer = require("./components/Footer");
+const Services = require("./components/Services");
+const Transactions = require("./components/Transactions");
 
-const App = () => (
-  <div className="min-h-screen">
-    <div className="gradient-bg-welcome">
-      <Navbar />
-      <Welcome />
-    </div>
-    <Services />
-    <Transactions />
-    <Footer />
-  </div>
-);
+const App = () =>
+  React.createElement(
+    "div",
+    { className: "min-h-screen" },
+    React.createElement(
+      "div",
+      { className: "gradient-bg-welcome" },
+      React.createElement(Navbar, null),
+      React.createElement(Welcome, null)
+    ),
+    React.createElement(Services, null),
+    React.createElement(Transactions, null),
+    React.createElement(Footer, null)
+  );
 
-export default App;
+module.exports = App;
