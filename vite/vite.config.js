153b707a9da5f032e8ff50f54c18f53922ee65fd
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-08-30 19:09:59 +0530

    Initial commit

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+});
