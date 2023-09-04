270b40f69043fd0b5c0a3688abbb7b1290cb75d6
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-04 16:08:47 +0530

    Initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-// import logo from "../../images/logo.svg";
 import logo from "../../images/logo.svg";
 
 const NavBarItem = ({ title, classprops }) => (
