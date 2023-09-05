6acf906ee6db5dd08935d4e0c3cd8f4c5f4cb884
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-05 17:48:42 +0530

    Initial commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x376902114964B34A88E5fdD7eC34113178A14fa2";
 export const contractABI = abi.abi;
