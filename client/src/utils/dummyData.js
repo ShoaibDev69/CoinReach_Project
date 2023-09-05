2fed42c4f37eeeff966d3c62b5a1cf92d1902e06
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-05 17:50:08 +0530

    Initial commit

--- client/src/utils/dummyData.js
+++ client/src/utils/dummyData.js
@@ -39,7 +39,7 @@ export default [
     id: 5,
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDh0emI4dG9wZnowMDgxOXR0Z29idXZ3Z3UxNWpraHgzMnRvcm5wayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TmTOZlAYc2vnSoQe64/giphy-downsized-large.gif",
     message: "",
-    timestamp: "17/08/2023, 4:33:21 PM",
+    timestamp: "17/08/2023, 4:39:21 PM",
     addressFrom: "0x376902114964B34A88E5fdD7eC34113178A14fa2",
     amount: "0.06",
     addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
