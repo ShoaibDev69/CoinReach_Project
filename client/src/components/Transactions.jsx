fadd3aed911b035473ab42592b3549e4555f669d
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-08-17 18:01:24 +0530

    Initial commit

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -80,7 +80,7 @@ const Transactions = () => {
           </h3>
         ) : (
           <h3 className="text-white text-3xl text-center my-2">
-            Connect your account to see the latest transactions
+            Connect your metamask wallet to see the latest transactions
           </h3>
         )}
 
